c787c30ad6be071e4fe9d62cb99697a1396dd924
Aaron(朱柏綸) <aaron@dynacolor.com.tw>
2026-03-27 15:20:59 +0800

    Add model to package data

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="PVAR_demo",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     package_data={
-        "PVAR_demo": ["samples/*/*.png"]
+        "PVAR_demo": ["samples/*/*.png", "models/*/*.onnx", "models/*/*.pt"]
     },
     include_package_data=True,
     install_requires=[
